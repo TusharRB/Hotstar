28523c416278de61cf39d3cac43a8f074166565e
Tushar Botare <tusharbotare17@gmail.com>
2023-05-05 18:18:19 +0530

    1st Commit

--- src/main/java/com/driver/services/ProductionHouseService.java
+++ src/main/java/com/driver/services/ProductionHouseService.java
@@ -1,4 +1,4 @@
-package com.driver.services;
+/*package com.driver.services;
 
 
 import com.driver.EntryDto.ProductionHouseEntryDto;
@@ -16,6 +16,41 @@ public class ProductionHouseService {
     public Integer addProductionHouseToDb(ProductionHouseEntryDto productionHouseEntryDto){
 
         return  null;
+    }
+
+
+
+}*/
+package com.driver.services;
+
+
+import com.driver.EntryDto.ProductionHouseEntryDto;
+import com.driver.model.ProductionHouse;
+import com.driver.model.WebSeries;
+import com.driver.repository.ProductionHouseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductionHouseService {
+
+    @Autowired
+    ProductionHouseRepository productionHouseRepository;
+
+    public Integer addProductionHouseToDb(ProductionHouseEntryDto productionHouseEntryDto){
+
+        ProductionHouse productionHouse = new ProductionHouse();
+
+        productionHouse.setName(productionHouseEntryDto.getName());
+        productionHouse.setRatings(0);
+
+        List<WebSeries> list = new ArrayList<>();
+        productionHouse.setWebSeriesList(list);
+        return  productionHouseRepository.save(productionHouse).getId();
+        //  return  null;
     }
 
 
--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -1,4 +1,4 @@
-package com.driver.services;
+/*package com.driver.services;
 
 
 import com.driver.model.Subscription;
@@ -35,6 +35,72 @@ public class UserService {
 
 
         return null;
+    }
+
+
+}
+
+ */
+package com.driver.services;
+
+
+import com.driver.model.Subscription;
+import com.driver.model.SubscriptionType;
+import com.driver.model.User;
+import com.driver.model.WebSeries;
+import com.driver.repository.UserRepository;
+import com.driver.repository.WebSeriesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    WebSeriesRepository webSeriesRepository;
+
+
+    public Integer addUser(User user){
+
+        //Jut simply add the user to the Db and return the userId returned by the repository
+
+        return userRepository.save(user).getId();
+    }
+
+    public Integer getAvailableCountOfWebSeriesViewable(Integer userId){
+
+        //Return the count of all webSeries that a user can watch based on his ageLimit and subscriptionType
+        //Hint: Take out all the Webseries from the WebRepository
+        User user = userRepository.findById(userId).get();
+        List<WebSeries> webSeries = webSeriesRepository.findAll();
+        int ans = 0;
+        for (WebSeries webs : webSeries)
+        {
+            if(webs.getAgeLimit() <= user.getAge()&& user.getSubscription().getSubscriptionType()==SubscriptionType.ELITE)
+            {
+                ans++;
+            }
+            else if(webs.getAgeLimit() <= user.getAge()&& user.getSubscription().getSubscriptionType()==SubscriptionType.PRO &&
+                    (webs.getSubscriptionType()==SubscriptionType.PRO || webs.getSubscriptionType()==SubscriptionType.BASIC))
+            {
+                ans++;
+            }
+            else if(webs.getAgeLimit() <= user.getAge()&& user.getSubscription().getSubscriptionType()==SubscriptionType.BASIC &&
+                    webs.getSubscriptionType()==SubscriptionType.BASIC)
+            {
+                ans++;
+            }
+        }
+
+
+
+        return ans;
     }
 
 
